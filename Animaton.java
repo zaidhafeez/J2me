eca87231e33c2dec69f188456023634a77ec0edb
zaidhafeez <67195919+zaidhafeez@users.noreply.github.com>
2021-03-18 18:22:19 +0530

    Add files via upload

--- Animaton.java
+++ Animaton.java
@@ -5,10 +5,11 @@
  */
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.microedition.midlet.*;
 import javax.microedition.lcdui.*;
 import javax.microedition.lcdui.game.GameCanvas;
-import javax.microedition.lcdui.game.Layer;
 import javax.microedition.lcdui.game.LayerManager;
 import javax.microedition.lcdui.game.Sprite;
 import javax.microedition.lcdui.game.TiledLayer;
@@ -17,22 +18,52 @@ import javax.microedition.lcdui.game.TiledLayer;
 /**
  * @author ZMQ-10
  */
-public class Animaton extends MIDlet {
+public class Animaton extends MIDlet implements CommandListener{
     
     Display display;
+    Form form;
+    ImageItem splash;
+    Command strtCmd, exitCmd;
     AGameCanvas aGameCanvas;
+   
+    BoxInterval boxInterval;
     
     public Animaton() throws IOException{
         
         display = Display.getDisplay(this);
-        aGameCanvas = new AGameCanvas(this);
+        aGameCanvas = new AGameCanvas(display, form);
         
+        form = new Form("Play Anmation");
+        
+        strtCmd = new Command("Start", Command.OK, 1);
+        exitCmd = new Command("Exit", Command.EXIT, 1);
+        form.append(new Spacer(50, 100));
+        
+        splash = new ImageItem(null, null, Item.LAYOUT_CENTER, null);
+        
+        try{
+            
+            Image image = Image.createImage("/splash1.png");
+            splash.setImage(image);
+         
+        }
+        catch(IOException e){
+            
+            System.out.println(e.toString());
+            
+        }
+        
+        form.append(splash);
+        form.addCommand(strtCmd);
+        form.addCommand(exitCmd);
+        form.setCommandListener(this);
         
     }
 
     public void startApp() {
         
-        display.setCurrent(aGameCanvas);
+//       AudioMidlet au = new AudioMidlet();
+        display.setCurrent(form);
         
     }
     
@@ -41,19 +72,60 @@ public class Animaton extends MIDlet {
     
     public void destroyApp(boolean unconditional) {
     }
+    
+    public void commandAction(Command command, Displayable displayable){
+        
+        if(command == strtCmd){
+            
+            form.deleteAll();
+            try {
+                
+                aGameCanvas = new AGameCanvas(display, form);
+                
+            } catch (IOException ex) {
+                
+                ex.printStackTrace();
+                
+            }
+            
+            display.setCurrent(aGameCanvas);
+//            System.out.println("Start Command");
+            
+        }
+        else if(command == exitCmd){
+            
+            try{
+                
+                destroyApp(true);
+                notifyDestroyed();
+                
+            }
+            catch(Exception e){
+                
+                System.out.println(e.toString());
+                
+            }
+            
+        }
+        
+    }
 }
 class AGameCanvas extends GameCanvas implements Runnable{
     
     Animaton animaton;
+    BoxInterval boxInterval;
+    Timer timer;
+    private Display display;
+    private Form form;
+    
     private boolean mtrucking;
     
     private LayerManager mLayerManager;
     
-    private TiledLayer mAtmosphere;
     private TiledLayer mBackground;
-    private int mAnimatedIndex;
     
-    private Sprite hero;
+    private Sprite bob;
+    private Sprite amir;
     private int mState, mDirection;
     
     private static final int kStanding = 1;
@@ -63,103 +135,94 @@ class AGameCanvas extends GameCanvas implements Runnable{
     
     private static final int[] kRunningSequence = {0, 1, 2};
     private static final int[] kStandingSequence = {3};
+    int count = 0;
+    
+    int x1, y1,x2,y2,x3,y3;
     
     Thread thread;
     
-    public AGameCanvas(Animaton animaton) throws IOException{
+    public AGameCanvas(Display start, Form stform) throws IOException{
         
         super(true);
-        this.animaton = animaton;
-        
+
+        form = stform;
+        display = start;
         
         mLayerManager = new LayerManager();
         int w = getWidth();
         int h = getHeight();
-        mLayerManager.setViewWindow(96, 0, w, h);
+        mLayerManager.setViewWindow((getWidth() / 3), 0, w, h);
         
         mtrucking = true;
         
-        createAtmosphere();
+        thread = new Thread(this, "Game Canvas");
+        thread.start();
         createBackground();
         createHero();
         
-        thread = new Thread(this, "Game Canvas");
-        thread.start();
+       
+
     }
     
     private void createBackground() throws IOException{
+        
         Graphics g = getGraphics();
-        Image backgroundImage = Image.createImage("/11.jpg");
+        Image backgroundImage = Image.createImage("/background.png");
         
-        int[] map = {
-        5, 1, 1, 4, 1, 1, 1, 1, 1, 6,
-        5, 1, 3, 1, 1, 3, 1, 1, 1, 6,
-        5, 1, 2, 1, 1, 2, 1, 1, 1, 6,
-        5, 1, 2, 3, 1, 2, 1, 1, 1, 6,
-        5, 1, 4, 2, 1, 2, 1, 1, 1, 6,
-        5, 1, 1, 4, 1, 2, 1, 1, 1, 6,
-        5, 1, 1, 1, 1, 4, 1, 1, 1, 6,
-        5, 1, 1, 1, 1, 1, 1, 1, 1, 6,
-        5, 1, 1, 1, 1, 1, 1, 1, 1, 6,
-        5, 1, 1, 1, 1, 1, 1, 1, 1, 6   
-    };
-        
+        int[][] map =  {{1,2,3,4,5,6,7,8,9,10},
+                       {11,12,13,14,15,16,17,18,19,20},
+                       {21,22,23,24,25,26,27,28,29,30},
+                       {31,32,33,34,35,36,37,38,39,40},
+                       {41,42,43,44,45,46,47,48,49,50},
+                       {51,52,53,54,55,56,57,58,59,60},
+                       {61,62,63,64,65,66,67,68,69,70},
+                       {71,72,73,74,75,76,77,78,79,80},
+                       {81,82,83,84,85,86,87,88,89,90},
+                       {91,92,93,94,95,96,97,98,99,100},
+        };
         
         mBackground = new TiledLayer(10, 10, backgroundImage, 32, 32);
-//        System.out.println(backgroundImage.getWidth());
-//        System.out.println(backgroundImage.getHeight());
-        
-        mBackground.setPosition(12, 0);
-//        System.out.println(mBackground.getCell(5, 3));
-        for(int i = 0; i < map.length; i++){
-            System.out.println(map.length);
-            int column = i % 10;
-            int row = (i - column) / 10;
-//            mBackground.paint(g);
+        mBackground.setPosition(0, 0);
+        mBackground.paint(g);
+
+        for (int row=0; row<10; row++) {
+            for (int col=0; col<10; col++) {
+
+                mBackground.setCell(col, row, map[row][col]);
             
-            mBackground.setCell(column, row, map[i]);
+            }
             
-        }
-        
-        mAnimatedIndex = mBackground.createAnimatedTile(10);
-        mBackground.setCell(3, 0, mAnimatedIndex);
-        mBackground.setCell(5, 0, mAnimatedIndex);
         mLayerManager.append(mBackground);
-    }
-    
-    private void createAtmosphere() throws IOException{
         
-        Image atmosphereImage = Image.createImage("/sky.png");
-        mAtmosphere = new TiledLayer(8, 1, atmosphereImage, atmosphereImage.getWidth(), atmosphereImage.getHeight());
-        mAtmosphere.fillCells(0, 0, 8, 1, 1);
-        mAtmosphere.setPosition(0, 192);
-        mLayerManager.insert(mAtmosphere, 0);
+        }
         
     }
     
     private void createHero() throws IOException{
-        
+
+        Graphics g = getGraphics();
         Image heroImage = Image.createImage("/hero.png");
-        hero = new Sprite(heroImage, 48, 48);
-        hero.setPosition(96 + (getWidth() - 48) / 2, 192);
-        hero.defineReferencePixel(24, 24);
+
+        bob = new Sprite(heroImage, 48, 48);
+        bob.setPosition((getWidth()/3), 192);
+        bob.defineReferencePixel(heroImage.getWidth() / 2, heroImage.getHeight() / 2);
         setDirection(kLeft);
         setState(kStanding);
-        mLayerManager.insert(hero, 1);
-        
+       
+        mLayerManager.insert(bob, 0);
+       
     }
-    
+   
     public void run(){
         
         int w = getWidth();
         int h = getHeight();
         Graphics graphics = getGraphics();
-        int frameCount = 0;
-        int factor = 2;
-        int animatedDelta = 0;
-        
-        
+
         while(mtrucking){
+           
+            checkBoundary();
+            
             
             if(isShown()){
                 
@@ -169,18 +232,17 @@ class AGameCanvas extends GameCanvas implements Runnable{
                     
                     setDirection(kLeft);
                     setState(kRunning);
-                    mBackground.move(3, 0);
-                    mAtmosphere.move(3, 0);
-                    hero.nextFrame();
+                    mBackground.move(-3, 0);
+                    bob.nextFrame();
+                    
                     
                 }
                 else if((keyStates & RIGHT_PRESSED) != 0){
                     
-                    setDirection(kRight);
+                    setDirection(kLeft);
                     setState(kRunning);
                     mBackground.move(-3, 0);
-                    mBackground.move(-3, 0);
-                    hero.nextFrame();
+                    bob.nextFrame();
                     
                 }
                 else{
@@ -189,28 +251,14 @@ class AGameCanvas extends GameCanvas implements Runnable{
                     
                 }
                 
-                frameCount++;
-                if(frameCount % factor == 0){
-                    
-                    int delta = 1;
-                    if(frameCount / factor < 10) delta = -1;
-                    mAtmosphere.move(delta, 0);
-                    if(frameCount / factor == 20) frameCount = 0;
-                    mBackground.setAnimatedTile(mAnimatedIndex, 8 + animatedDelta++);
-                    if(animatedDelta == 3) animatedDelta = 0;
-                    
-                }
-                
                 graphics.setColor(0x5b1793);
                 graphics.fillRect(0, 0, w, h);
                 
                 mLayerManager.paint(graphics, 0, 0);
                 
-                flushGraphics();
-                
             }
             
-            int delayOfLoop = 1000 / 25;
+            int delayOfLoop = 1000 /8;
             long startLoopTime = System.currentTimeMillis();
             
             long endLoopTime = System.currentTimeMillis();
@@ -238,12 +286,12 @@ class AGameCanvas extends GameCanvas implements Runnable{
         if(newDirection == mDirection)return;
         if(mDirection == kLeft){
             
-            hero.setTransform(Sprite.TRANS_MIRROR);
+            bob.setTransform(Sprite.TRANS_MIRROR);
             
         }
         else if(mDirection == kRight){
             
-            hero.setTransform(Sprite.TRANS_NONE);
+            bob.setTransform(Sprite.TRANS_NONE);
             
         }
         mDirection = newDirection;
@@ -256,11 +304,11 @@ class AGameCanvas extends GameCanvas implements Runnable{
         switch(newState){
             
             case kStanding:
-                hero.setFrameSequence(kStandingSequence);
-                hero.setFrame(0);
+                bob.setFrameSequence(kStandingSequence);
+                bob.setFrame(0);
                 break;
             case kRunning:
-                hero.setFrameSequence(kRunningSequence);
+                bob.setFrameSequence(kRunningSequence);
                 break;
             default:
                 break;
@@ -284,7 +332,115 @@ class AGameCanvas extends GameCanvas implements Runnable{
 //        return layer.isVisible();
 //        
 //    }
+    
+    public boolean checkBoundary(){
+        Image amirImage;
+        Graphics g = getGraphics();
         
+        if(mBackground.getX() + getWidth() < getWidth()){
+            
+            mBackground.setPosition(getWidth()/3, 0);
+            count++;   
+            
+           
+            
+        }
+        if(count == 2){
+           
+             
+            try {
+
+                amirImage = Image.createImage("/hero.png");
+                amir = new Sprite(amirImage, 48, 48);
+
+                amir.setPosition(getWidth() + 96, 192);
+                mLayerManager.insert(amir, 0);
+                amir.setTransform(kRight);
+
+
+
+                boxInterval = new BoxInterval(this);
+                timer = new Timer();
+                timer.schedule(boxInterval, 500, 4000); 
+
+                try{
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.toString();
+                }
+
+
+            } catch (Exception e) {
+
+                e.toString();
+
+            }
+           
+      
+       }
+//        try{
+//            Thread.sleep(100000);
+//        } catch (Exception e) {
+//            e.toString();
+//        }
+       flushGraphics();
+       
+        return true;
+    }
+    
+
+    public void trianglePosition(){
+        
+        x1 = getWidth() / 2; 
+        y1 = getHeight() / 2 + getHeight() / 8;
+        x2 = getWidth() / 2 + getWidth() / 6;
+        y2 = getHeight() / 2 + getHeight() / 8;
+        x3 = (getWidth() / 2) + (getWidth() / 4);
+        y3 =  3 * (getHeight() / 4); 
+        
+    }
+    
+    public void paint(Graphics g){
+        
+        g.setColor(255,0,0);
+        g.fillRect(getWidth() / 2, getHeight() / 2 , getWidth()/2, getHeight()/8);
+        g.fillTriangle(x1, y1, x2, y2, x3, y3);
+        g.setColor(0, 255, 0);
+        String bobMsg = "Hello How Are You";
+        g.drawString(bobMsg, getWidth() / 2 + 5, getHeight() / 2 + 2, Graphics.TOP | Graphics.LEFT);
+        
+    }
+      
+}
+class BoxInterval extends TimerTask{
+    AGameCanvas aGameCanvas;
+    
+    public BoxInterval(AGameCanvas aGameCanvas)
+    {
+        this.aGameCanvas=aGameCanvas;
+       
+    }
+
+    public void run() {
+        
+        aGameCanvas.trianglePosition();
+        aGameCanvas.repaint();
+       
+        try {
+            
+            Thread.sleep(1500);
+            aGameCanvas.x3 = aGameCanvas.getWidth()/4;
+            aGameCanvas.y3 = 3 * aGameCanvas.getHeight() / 4;
+            aGameCanvas.repaint();
+            
+        } catch (InterruptedException ex) {
+            
+            ex.printStackTrace();
+            
+        }
+        
+    }
+    
 }
     
 
